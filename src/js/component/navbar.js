2384cf67175ea73a894a79ac03be93328fefa6f5
SergioCoto <coto.sergio@gmail.com>
2021-04-13 03:14:13 +0000

    Terminado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import * as Scroll from "react-scroll";
+
+import { Favorites } from "./favorites.js";
+
+import { ScrollingProvider, Section } from "react-scroll-section";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<nav className="navbar navbar-light bg-light m-1 pl-5">
+			<div className="container main p-1 m-0">
+				<Link to="/">
+					<img src="https://cdn.iconscout.com/icon/free/png-512/starwars-5-555964.png" />
 				</Link>
 			</div>
+			<Favorites />
 		</nav>
 	);
 };
